e1117f73a277c0c4abcc1e723faef740a125431d
Gideon-Safo <gjsafo002@st.ug.edu.gh>
2026-09-05 10:11:22 +0000

    Completed Task 2- Abstract Classes and Methods

---
+++ ShapeDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ShapeDemo
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override double GetArea()
+        {
+            return Length * Width;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            Rectangle rectangle = new Rectangle(6, 4);
+
+            Console.WriteLine($"Circle Area: {circle.GetArea():F2}");
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+        }
+    }
+}
